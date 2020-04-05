3d910a94a824ded650eab32b6d1679e402ff782f
dechariot <nsprisang@gmail.com>
2020-04-05 19:56:07 +0700

    fix url path error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,13 +8,13 @@ export default function NavBar() {
         <Container fluid className="d-flex mr-0 p-0">
             <Container className="p-0 ml-2 d-flex align-items-center navbar-left">
                 {/* <div id="nav-logo">                
-                    <img src={logo}/>
+                    <img src={logo}/> 
                 </div> */}
-                <a  className="font-weight-bold" style={{ margin: '0px 0px 0px 5px' }} href="/" >dechariot</a>
+                <a  className="font-weight-bold" style={{ margin: '0px 0px 0px 5px' }} href={'/'} >dechariot</a>
             </Container>
             <Container className="mr-0 d-flex align-items-center justify-content-end navbar-right">
-                <a className="p-3" href="/projects">Projects</a>
-                <a p-className="p-3" href="/about">About</a>
+                <a className="p-3" href={'/projects'}>Projects</a>
+                <a p-className="p-3" href={'/about'}>About</a>
             </Container>
         </Container>
     )
